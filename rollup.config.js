efc5ceeb89b91e2941483a3ccdf0e7ea9baae22a
Daniel Imfeld <daniel@imfeld.dev>
2020-12-15 20:03:32 +0000

    emitCss false for non-Svelte build

--- rollup.config.js
+++ rollup.config.js
@@ -3,22 +3,23 @@ import resolve from '@rollup/plugin-node-resolve';
 import pkg from './package.json';
 
 const name = pkg.name
-	.replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
-	.replace(/^\w/, m => m.toUpperCase())
-	.replace(/-\w/g, m => m[1].toUpperCase());
+  .replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
+  .replace(/^\w/, (m) => m.toUpperCase())
+  .replace(/-\w/g, (m) => m[1].toUpperCase());
 
 export default {
-	input: 'src/index.js',
-	output: [
-		{ file: pkg.module, 'format': 'es' },
-		{ file: pkg.main, 'format': 'umd', name }
-	],
-	plugins: [
-		svelte({
-			compilerOptions: {
-				dev: process.env.NODE_ENV === 'development',
-			}
-		}),
-		resolve()
-	]
+  input: 'src/index.js',
+  output: [
+    { file: pkg.module, format: 'es' },
+    { file: pkg.main, format: 'umd', name },
+  ],
+  plugins: [
+    svelte({
+      compilerOptions: {
+        dev: process.env.NODE_ENV === 'development',
+      },
+      emitCss: false,
+    }),
+    resolve(),
+  ],
 };
