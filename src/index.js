5e38ff2b20f1d74d8b939135a4aff5e564d10102
Daniel Imfeld <daniel@imfeld.dev>
2021-01-28 08:08:46 +0000

    forgot to export Breadcrumbs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as Zoomable } from './Zoomable.svelte';
 export { default as ZoomableContainer } from './ZoomableContainer.svelte';
+export { default as Breadcrumbs } from './Breadcrumbs.svelte';
 export { presets as zoomPresets } from './transition';
